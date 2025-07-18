ed42d1393ca36f61cd3e546228bb0a6f122ad35b
JavierMilan <jamilanfe@gmail.com>
2025-07-18 19:15:04 +0200

    adelantando

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 const pantalla = document.querySelector(".pantalla");
 const botones = document.querySelectorAll(".boton");
- 
-botones.forEach(boton =>{
-  boton.addEventListener("click" ,() =>{
+
+botones.forEach(boton => {
+  boton.addEventListener("click" ,()=>{
     const teclaPulsada = boton.textContent;
 
-    if(teclaPulsada === "C"){
+    if(boton.id === "limpiar"){
       pantalla.textContent = "0";
       return;
     }
+
     if(boton.id === "borrar"){
-      if(pantalla.textContent.length === 1 || pantalla.textContent === "error"){
+      if(pantalla.textContent.length === 1){
         pantalla.textContent = "0";
       }else{
-        pantalla.textContent = pantalla.textContent.slice(0, -1);
+        pantalla.textContent = pantalla.textContent.slice(0 , -1);
       }
       return;
     }
     if(boton.id === "igual"){
       try{
-        pantalla.textContent = eval(pantalla.textContent);
-      }catch{
+        pantalla.textContent = eval(pantalla.textContent.replace(/x/g, "*"));
+      } catch {
         pantalla.textContent = "error";
       }
       return;
     }
-    if(pantalla.textContent === "0" || pantalla.textContent === "error"){
+        if(pantalla.textContent === "0" || pantalla.textContent === "error"){
       pantalla.textContent = teclaPulsada;
     }else{
-
       pantalla.textContent += teclaPulsada;
     }
+  });
+});
 
-  })
-})
